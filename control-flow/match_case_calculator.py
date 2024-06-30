6b690e3aadd9baf69024d168504f9bc7e77cec28
Ephraim <122993960+EphraimMucale@users.noreply.github.com>
2024-06-30 23:29:02 +0200

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,2 +1,16 @@
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the first number:"))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operation = str(input("Choose operation (+, -, *, /): "))
+
+match operation:
+    case "+":
+        print(num1 + num2)
+    case "-":
+        print(num1 - num2)
+    case "*":
+        print(num1 * num2)
+    case "/":
+        if num2 == 0:
+           print("You cannot devide by zero")
+        else:
+            print(num1 / num2)
