e0b3916cd7893115b9cfc926dc0d7058a6eec6cb
Ephraim <122993960+EphraimMucale@users.noreply.github.com>
2024-06-30 23:51:23 +0200

    Create daily_reminder.py

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,3 @@
+task variable = input("Describe your task")
+priority variable = input("What is your task priority? (high, medium, low) ")
+time_bound variable = input("Is your task time-bound? (yes or no) ")
