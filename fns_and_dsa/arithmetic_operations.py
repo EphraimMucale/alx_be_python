bad2bed9f0d820d8abf292e1b8e0c52cdd9c2f95
Ephraim <122993960+EphraimMucale@users.noreply.github.com>
2024-07-12 16:42:29 +0200

    Update arithmetic_operations.py
    arithmetic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,23 @@
+def perform_operations(num1, num2, operation):
 
+    match operation:
+        case "add": return num1 + num2
+        case "subtract": return num1 - num2
+        case "multiply": return num1 * num2
+        case "devide": 
+            if (num1 == 0) or (num2 == 0):
+                return "Invalid division"
+            else:
+                return num1 / num2
+            
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operations(num1, num2, operation)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
