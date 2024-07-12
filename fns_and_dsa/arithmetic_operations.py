2a4a69f0c9fb87bdffd5fa010d17fc1edb6f05d9
Ephraim <122993960+EphraimMucale@users.noreply.github.com>
2024-07-12 16:44:40 +0200

    Update arithmetic_operations.py
    corrections

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operations(num1, num2, operation):
+def perform_operation(num1, num2, operation):
 
     match operation:
         case "add": return num1 + num2
@@ -16,7 +16,7 @@ def main():
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
-    result = perform_operations(num1, num2, operation)
+    result = perform_operation(num1, num2, operation)
     print(f"Result: {result}")
 
 if __name__ == "__main__":
